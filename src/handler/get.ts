3f406491984d29097d59486c9d87cfb0d5d096e0
naathanbrown <nathancjb@googlemail.com>
2023-12-21 15:28:54 +0000

    feat(cb2-9857): create inital lambda

--- src/handler/get.ts
+++ src/handler/get.ts
@@ -1,24 +1,34 @@
+import { GetParameterCommand, SSMClient } from '@aws-sdk/client-ssm';
+import type { APIGatewayProxyResult } from 'aws-lambda';
 import 'dotenv/config';
-import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import logger from '../util/logger';
 
 // eslint-disable-next-line @typescript-eslint/require-await
-export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  if (!event.queryStringParameters) {
+export const handler = async (): Promise<APIGatewayProxyResult> => {
+  try {
+    logger.info('min version endpoint called');
+    let minVersion;
+
+    // cannot call ssm locally so this is a workaround.
+    if (process.env.AWS_SAM_LOCAL) {
+      minVersion = '1.0';
+    } else {
+      const client = new SSMClient();
+      const command = new GetParameterCommand({ Name: 'fill me in', WithDecryption: false });
+      const response = await client.send(command);
+      logger.info('response from ssm');
+      minVersion = response.Parameter?.Value;
+    }
+
     return {
-      statusCode: 400,
-      body: JSON.stringify({
-        message: 'Missing query string parameters',
-      }),
+      statusCode: 200,
+      body: JSON.stringify(minVersion),
+    };
+  } catch (error) {
+    logger.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify('Error fetching min version'),
     };
   }
-
-  const queryParams = event.queryStringParameters;
-
-  logger.info(queryParams);
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ queryParams }),
-  };
 };
