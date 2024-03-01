5af617568f40e2bda471d28ee995bb2a70fd6476
Matt Waldron <matt.waldron@bjss.com>
2024-03-01 15:20:17 +0000

    feat(cb2-10573): add the feature flag endpoint

---
+++ src/feature-flags/get.ts
@@ -0,0 +1,41 @@
+import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import 'dotenv/config';
+import logger from '../util/logger';
+import { Client, Clients } from './util/app-config';
+import { headers } from '../util/headers';
+
+const parseClientFromEvent = (value: string = ''): Client => {
+  const clientValue = value.toUpperCase();
+  return Client[clientValue as keyof typeof Client];
+}
+
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  logger.info('feature flag endpoint called');
+
+  const client = parseClientFromEvent(event?.pathParameters?.client);
+  if (client === undefined) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify(`Client not found: ${event?.pathParameters?.client}`),
+      headers,
+    };
+  }
+
+  try {
+    const appConfig = Clients.get(client);
+    const flags = await appConfig!()!;
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(flags),
+      headers,
+    };
+  } catch (error) {
+    logger.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify('Error fetching feature flags'),
+      headers,
+    };
+  }
+};
---
+++ tests/feature-flags/feature-flags.test.ts
@@ -0,0 +1,70 @@
+const getAppConfig = jest.fn();
+
+jest.mock('@aws-lambda-powertools/parameters/appconfig', () => ({
+  getAppConfig: getAppConfig
+}));
+
+import { handler } from '../../src/feature-flags/get';
+import { headers } from '../../src/util/headers';
+import { APIGatewayProxyEvent } from 'aws-lambda';
+
+describe('feature flags endpoint', () => {
+  const validEvent = {
+    pathParameters: {
+      client: 'vtx'
+    }
+  } as unknown as APIGatewayProxyEvent;
+
+  type CvsFeatureFlags = {
+    firstFlag: Flag,
+    secondFlag: Flag,
+  }
+
+  type Flag = {
+    enabled: boolean
+  }
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should return 404 when an invalid client is specified', async () => {
+    const result = await handler({
+      pathParameters: {
+        client: 'non-existent client'
+      }
+    } as unknown as APIGatewayProxyEvent);
+
+    expect(result).toEqual({ statusCode: 404, body: '\"Client not found: non-existent client\"', headers });
+    expect(getAppConfig).toHaveBeenCalledTimes(0);
+  });
+
+  it('should return 500 when app config throws an error', async () => {
+    getAppConfig.mockRejectedValue('Error: timeout');
+
+    const result = await handler(validEvent);
+
+    expect(result).toEqual({ statusCode: 500, body: '\"Error fetching feature flags\"', headers });
+    expect(getAppConfig).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return feature flags from app config', async () => {
+    const body = {
+      firstFlag: {
+        enabled: true
+      },
+      secondFlag: {
+        enabled: false
+      },
+    };
+    getAppConfig.mockReturnValue(body);
+
+    const result = await handler(validEvent);
+    const flags = JSON.parse(result.body) as CvsFeatureFlags;
+
+    expect(result).toEqual({ statusCode: 200, body: JSON.stringify(body), headers });
+    expect(flags.firstFlag.enabled).toEqual(true);
+    expect(flags.secondFlag.enabled).toEqual(false);
+    expect(getAppConfig).toHaveBeenCalledTimes(1);
+  });
+});
